24084254d961f01c38383cd690a746915d0465c2
moulay EL ALAOUI <moulay-lahbib.el-alaoui@campus-centre.fr>
2023-03-02 10:27:20 +0100

    première et seconde manipulation du cours poo

---
+++ manipulation_01/assure.php
@@ -0,0 +1,27 @@
+<?php
+
+class Assure {
+    // 1 - déclaration des propriètés (attributs)
+    private $nom = 'Jules';
+    private $age = '34';
+    private $domicile = 'Paris';
+    private $bonusMalus = 0;
+
+    // 2 - définir les méthodes
+
+    public function reglerAssurance(){
+
+
+    }
+    public function avoirAccident(){
+
+
+    }
+    public function parrainer(){
+
+
+    }
+
+
+
+}
